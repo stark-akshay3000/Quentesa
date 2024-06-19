cf81cafc5cfd23147478b36fe21601782324396f
stark <akshaystark3000@gmail.com>
2024-06-19 16:15:10 +0530

    databas and models

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,30 @@
+import mongoose , {Mongoose} from 'mongoose'
+
+const MONOGDB_URL = process.env.MONGODB_URL;
+interface MongooseConnection{
+    conn:Mongoose|null;
+    promise:Promise<Mongoose> | null
+
+
+}
+
+let cached:MongooseConnection= (global as any).mongoose
+
+if(!cached){
+    cached = (global as any).mongoose={
+        conn:null,promise:null
+    }
+}
+
+export const connectToDatabase = async()=>{
+    if(cached.conn )return cached.conn
+
+    if(!MONOGDB_URL) throw new Error("Missing mongodb url")
+
+        cached.promise =cached.promise || 
+        mongoose.connect(MONOGDB_URL,
+            {dbName:'Quentesa',bufferCommands:false})
+
+      cached.conn = await cached.promise;
+      return cached.conn;      
+}
