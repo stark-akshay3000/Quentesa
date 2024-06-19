762ec5cd712cbfd81c18de870434c27ca4f5f062
stark <akshaystark3000@gmail.com>
2024-06-19 22:50:22 +0530

    deployement error resolved

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,6 @@
+
 /* eslint-disable camelcase */
-import clerkClient  from "@clerk/nextjs";
+import clerkClient from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
@@ -8,6 +9,7 @@ import { Webhook } from "svix";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
