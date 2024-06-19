8e26c74c5cc063ac38354770f0a4b864f2027fdd
stark <akshaystark3000@gmail.com>
2024-06-19 22:09:33 +0530

    webhooks added

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import {authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({});
+export default authMiddleware({
+
+  publicRoutes:['/api/webhooks/clerk']
+});
+
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
